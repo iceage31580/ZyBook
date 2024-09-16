9bc39b1ffd89205a12105347a2f6c90cf397a52c
iceage31580 <75943275+iceage31580@users.noreply.github.com>
2024-09-16 04:41:34 -0700

    Add files via upload

---
+++ 2.15.1 One large program.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std; 
+
+int main(){
+ cout << "Step 1 complete" << endl; 
+ cout << "Step 2 as well" << endl; 
+ cout << "All steps now complete" << endl; 
+
+return 0; 
+} 
+
